84765b7d7188bde2fba81498e77c6ecb9108565f
Prof. Carlos Boni <carlosboni.niklison@gmail.com>
2023-04-26 14:34:56 +0200

    ta-te-ti

---
+++ ta-te-ti/ta-te-ti.js
@@ -0,0 +1,15 @@
+// juego del ta-te-ti o tres en raya
+// nivel basico
+// el usuario pinta 3 elementos y el programa dice si ganó o no
+
+function elige(fil,col){
+
+}
+
+function pintar(fil,col){
+
+}
+
+function restaurar(fil,col){
+
+}
